ffb6a07607828bb6b0cf18de027a506080b72405
RaphaelXZA <108958428+RaphaelXZA@users.noreply.github.com>
2024-07-20 19:28:52 -0500

    Cesar hola

--- GameJam2024_ManatiDefender/Assets/ScreenCalculateInstance.cs
+++ GameJam2024_ManatiDefender/Assets/ScreenCalculateInstance.cs
@@ -9,6 +9,8 @@ namespace kelp_eater
         public GameObject[] objectPrefabs; // Prefabricado del objeto que quieres instanciar
         public GameObject[] squarePrefabs; // Prefabricado del cuadrado que quieres instanciar en la parte superior
         public GameObject[] voids;
+        public GameObject[] rocks;
+
         public int numberOfObjects = 5; // Número de objetos que quieres instanciar
         public float marginTop = 0.5f; // Margen en unidades de World Space desde el borde superior de la pantalla
         public float marginBottom = 0.5f; // Margen en unidades de World Space desde el borde inferior de la pantalla
@@ -18,6 +20,7 @@ namespace kelp_eater
             ArrangeObjects();
             ArrangeSquares();
             ArrangeBottomObjects();
+            ArrangeBottomRocks();
         }
 
         void ArrangeObjects()
@@ -74,9 +77,32 @@ namespace kelp_eater
             for (int i = 0; i < numberOfObjects && i < voids.Length; i++)
             {
                 float posX = startX + i * spaceBetweenObjects;
-                Vector3 spawnPosition = new Vector3(posX, screenBottom + 3.6f, 0); 
+                Vector3 spawnPosition = new Vector3(posX, screenBottom + 3.2f, 0); 
 
                 GameObject objectToArrange = voids[i];
+
+                objectToArrange.transform.position = spawnPosition;
+            }
+        }
+
+        void ArrangeBottomRocks()
+        {
+            float screenHeightInWorldUnits = Camera.main.orthographicSize * 2;
+            float screenWidthInWorldUnits = screenHeightInWorldUnits * Camera.main.aspect;
+
+            float startX = -screenWidthInWorldUnits / 2 + marginBottom * 6;
+            float endX = screenWidthInWorldUnits / 2 - marginBottom * 6;
+
+            float spaceBetweenObjects = (endX - startX) / (numberOfObjects - 1);
+
+            float screenBottom = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane)).y;
+
+            for (int i = 0; i < numberOfObjects && i < rocks.Length; i++)
+            {
+                float posX = startX + i * spaceBetweenObjects;
+                Vector3 spawnPosition = new Vector3(posX, screenBottom + 0f, 0.09f);
+
+                GameObject objectToArrange = rocks[i];
 
                 objectToArrange.transform.position = spawnPosition;
             }
